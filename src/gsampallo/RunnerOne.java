cb4aaff76d13ec5fbb308abe7c89224e19c411ad
Guillermo Sampallo <guille@gmail.com>
2020-05-15 11:13:58 -0300

    Home

---
+++ src/gsampallo/RunnerOne.java
@@ -0,0 +1,12 @@
+package gsampallo;
+
+public class RunnerOne {
+
+    public RunnerOne() {
+        System.out.println("Runner One!");
+    }
+
+    public static void main(String[] args) throws Exception {
+        RunnerOne runnerOne = new RunnerOne();
+    }
+}
